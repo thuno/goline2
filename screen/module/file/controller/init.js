28682a37b56f387246e9c301b1604f3aa66a82ae
thuno <thuno2698@gmail.com>
2024-02-20 16:24:36 +0700

    cdn v-5e16b9a

--- screen/module/file/controller/init.js
+++ screen/module/file/controller/init.js
@@ -1,4 +1,4 @@
-$('body > #home-body').load('https://cdn.jsdelivr.net/gh/thuno/goline2@e1efdc8/project-component/loading.html', async function () {
-    $('body > #home-body').load('https://cdn.jsdelivr.net/gh/thuno/goline2@e1efdc8/screen/module/file/local-component/body-layout.html', function () {
+$('body > #home-body').load('https://cdn.jsdelivr.net/gh/thuno/goline2@5e16b9a/project-component/loading.html', async function () {
+    $('body > #home-body').load('https://cdn.jsdelivr.net/gh/thuno/goline2@5e16b9a/screen/module/file/local-component/body-layout.html', function () {
     })
 });
