30702d9eecdd3bf981ceb8077335a2f19573cc4f
thuno <thuno2698@gmail.com>
2024-02-21 17:53:11 +0700

    cdn v-b98954a

--- screen/module/file/controller/init.js
+++ screen/module/file/controller/init.js
@@ -1,11 +1,11 @@
 var left_view, right_view, layer_view, assets_view, design_view, prototype_view, state_view
-$('body > #body').load('https://cdn.jsdelivr.net/gh/thuno/goline2@ea46e70/project-component/loading.html', async function () {
+$('body > #body').load('https://cdn.jsdelivr.net/gh/thuno/goline2@b98954a/project-component/loading.html', async function () {
     // customerList()
     const pId = location.hash.match(/file\?id\=[\d]*/g)[0].replace('file?id=', "")
     const res = await ProjectDA.getByID(parseInt(pId))
     debugger
     await ProjectDA.getPermission()
-    $('body > #body').load('https://cdn.jsdelivr.net/gh/thuno/goline2@ea46e70/screen/module/file/local-component/body-layout.html', function () {
+    $('body > #body').load('https://cdn.jsdelivr.net/gh/thuno/goline2@b98954a/screen/module/file/local-component/body-layout.html', function () {
         permissionTool()
         left_view = document.getElementById('left_view')
         right_view = document.getElementById('right_view')
