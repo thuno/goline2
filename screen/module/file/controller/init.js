d3676eb3ad9592bc6089212a62a9d9ce6aa9b800
thuno <thuno2698@gmail.com>
2024-02-21 17:44:53 +0700

    cdn v-fae4686

--- screen/module/file/controller/init.js
+++ screen/module/file/controller/init.js
@@ -1,5 +1,5 @@
 var left_view, right_view, layer_view, assets_view, design_view, prototype_view, state_view
-$('body > #body').load('https://cdn.jsdelivr.net/gh/thuno/goline2@28682a3/project-component/loading.html', async function () {
+$('body > #body').load('https://cdn.jsdelivr.net/gh/thuno/goline2@fae4686/project-component/loading.html', async function () {
     // customerList()
     const pId = location.hash.match(/file\?id\=[\d]*/g)[0].replace('file?id=', "")
     await ProjectDA.getByID(parseInt(pId))
