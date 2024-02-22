f7ce1146ecd3283a92049f7c0947b9bfb3b4987f
thuno <thuno2698@gmail.com>
2024-02-22 14:59:29 +0700

    cdn v-4fb60a1

--- screen/module/home/controller/init.js
+++ screen/module/home/controller/init.js
@@ -1,4 +1,4 @@
-$('body > #home-body').load('https://cdn.jsdelivr.net/gh/thuno/goline2@d5a4ea5/project-component/loading.html', async function () {
+$('body > #home-body').load('https://cdn.jsdelivr.net/gh/thuno/goline2@4fb60a1/project-component/loading.html', async function () {
     const userItem = UserService.user()
     $('.user-container .user-name').text(userItem?.name ?? "-");
     $('.user-container .user-email').text(userItem?.email ?? "-");
@@ -9,7 +9,7 @@ $('body > #home-body').load('https://cdn.jsdelivr.net/gh/thuno/goline2@d5a4ea5/p
     // debugger
     const x = new Date()
     // ProjectDA.list = [{ ID: 43, Name: 'Test', DateUpdate: x }, { ID: 23, Name: 'sdfihishfoisf', DateUpdate: x }]
-    $('body > #home-body').load('https://cdn.jsdelivr.net/gh/thuno/goline2@d5a4ea5/screen/module/home/local-component/body-layout.html', function () {
+    $('body > #home-body').load('https://cdn.jsdelivr.net/gh/thuno/goline2@4fb60a1/screen/module/home/local-component/body-layout.html', function () {
         switch_tab_selected('recent')
     })
 });
