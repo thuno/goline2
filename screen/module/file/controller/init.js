19365926213e4d2a84fe8b0eacd8db54bb890689
thuno <thuno2698@gmail.com>
2024-02-23 19:01:11 +0700

    cdn v-aa01292

--- screen/module/file/controller/init.js
+++ screen/module/file/controller/init.js
@@ -18,7 +18,7 @@ var showF12 = false
 var design_view_index = 0
 const isMac = navigator.userAgent.indexOf('Mac OS X') != -1
 var select_component
-$('body > #body').load('https://cdn.jsdelivr.net/gh/thuno/goline2@ee0df5c/project-component/loading.html', async function () {
+$('body > #body').load('https://cdn.jsdelivr.net/gh/thuno/goline2@aa01292/project-component/loading.html', async function () {
     const pId = location.hash.match(/file\?id\=[\d]*/g)[0].replace('file?id=', "")
     await ProjectDA.getByID(parseInt(pId))
     const res = await ProjectDA.getPermission()
@@ -40,7 +40,7 @@ $('body > #body').load('https://cdn.jsdelivr.net/gh/thuno/goline2@ee0df5c/projec
             PageDA.checkEditPermission(PageDA.obj.Permission)
         }
     }
-    $('body > #body').load('https://cdn.jsdelivr.net/gh/thuno/goline2@ee0df5c/screen/module/file/local-component/body-layout.html', async function () {
+    $('body > #body').load('https://cdn.jsdelivr.net/gh/thuno/goline2@aa01292/screen/module/file/local-component/body-layout.html', async function () {
         permissionTool()
         customerList()
         left_view = document.getElementById('left_view')
