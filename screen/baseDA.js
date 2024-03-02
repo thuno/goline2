d0e162f36c8136fcd60abf485894ed89f1a2f48e
thuno <thuno2698@gmail.com>
2024-03-02 12:44:53 +0700

    .

--- screen/baseDA.js
+++ screen/baseDA.js
@@ -140,7 +140,7 @@ const uploadFile = async ({ listFile, docId }) => {
         for (let j = 0; j < sliceList.length; j++) {
             formData.append("files", sliceList[j]);
         }
-        let result = await BaseDA.postFile(ConfigApi.socketWiniFile + 'uploadfile', {
+        let result = await BaseDA.postFile(ConfigApi.socketWiniFile + '/uploadfile', {
             headers: headers,
             formData: formData
         })
