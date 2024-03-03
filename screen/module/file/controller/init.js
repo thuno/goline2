9590f69c0ebfcedb7dcc479d0488a5548bc94b1c
thuno <thuno2698@gmail.com>
2024-03-03 23:48:50 +0700

    cdn v-af9e347

--- screen/module/file/controller/init.js
+++ screen/module/file/controller/init.js
@@ -33,7 +33,7 @@ var showF12 = false
 var design_view_index = 0
 const isMac = navigator.userAgent.indexOf('Mac OS X') != -1
 var select_component
-$('body > #body').load('https://cdn.jsdelivr.net/gh/thuno/goline2@b1a94e0/project-component/loading.html', async function () {
+$('body > #body').load('https://cdn.jsdelivr.net/gh/thuno/goline2@af9e347/project-component/loading.html', async function () {
     const pId = location.hash.match(/file\?id\=[\d]*/g)[0].replace('file?id=', "")
     const pInforRes = await ProjectDA.getByID(parseInt(pId))
     if (pInforRes.Code === 200) {
@@ -60,7 +60,7 @@ $('body > #body').load('https://cdn.jsdelivr.net/gh/thuno/goline2@b1a94e0/projec
             }
         }
         let wbaseResponse = await WBaseDA.apiGetInitWbase()
-        $('body > #body').load('https://cdn.jsdelivr.net/gh/thuno/goline2@b1a94e0/screen/module/file/local-component/body-layout.html', async function () {
+        $('body > #body').load('https://cdn.jsdelivr.net/gh/thuno/goline2@af9e347/screen/module/file/local-component/body-layout.html', async function () {
             permissionTool()
             customerList()
             left_view = document.getElementById('left_view')
