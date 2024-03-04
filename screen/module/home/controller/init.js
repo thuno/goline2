f078ca475db0320ac93bddb940a4bd75b3ed4384
thuno <thuno2698@gmail.com>
2024-03-05 01:42:36 +0700

    cdn v-385dde2

--- screen/module/home/controller/init.js
+++ screen/module/home/controller/init.js
@@ -1,4 +1,4 @@
-$('body > #home-body').load('https://cdn.jsdelivr.net/gh/thuno/goline2@37aa8e3/project-component/loading.html', async function () {
+$('body > #home-body').load('https://cdn.jsdelivr.net/gh/thuno/goline2@385dde2/project-component/loading.html', async function () {
     TitleBarDA.updateTitleBar(0)
     const userItem = UserService.user()
     $('.user-container .user-name').text(userItem?.name ?? "-");
@@ -8,7 +8,7 @@ $('body > #home-body').load('https://cdn.jsdelivr.net/gh/thuno/goline2@37aa8e3/p
     await ProjectDA.init()
     await TeamDA.init()
     const x = new Date()
-    $('body > #home-body').load('https://cdn.jsdelivr.net/gh/thuno/goline2@37aa8e3/screen/module/home/local-component/body-layout.html', function () {
+    $('body > #home-body').load('https://cdn.jsdelivr.net/gh/thuno/goline2@385dde2/screen/module/home/local-component/body-layout.html', function () {
         switch_tab_selected('recent')
     })
 });
