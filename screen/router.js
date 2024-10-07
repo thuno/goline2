36e097502edc234a0663f8dbcba121e3f73fd3c1
thuno <thuno2698@gmail.com>
2024-10-07 12:16:53 +0700

    design view aacfce7

--- screen/router.js
+++ screen/router.js
@@ -7,28 +7,28 @@ if (UserService.token()) {
         document.head.querySelector('title').innerHTML = 'Home'
         let cssTag = document.createElement('link')
         cssTag.rel = "stylesheet"
-        cssTag.href = "https://cdn.jsdelivr.net/gh/thuno/goline2@cda4f66/screen/module/home/home.css"
+        cssTag.href = "https://cdn.jsdelivr.net/gh/thuno/goline2@aacfce7/screen/module/home/home.css"
         // cssTag.href = "./screen/module/home/home.css"
         document.head.appendChild(cssTag)
-        $('body').load('https://cdn.jsdelivr.net/gh/thuno/goline2@cda4f66/screen/module/home/home.html')
+        $('body').load('https://cdn.jsdelivr.net/gh/thuno/goline2@aacfce7/screen/module/home/home.html')
         // $('body').load('./screen/module/home/home.html')
     } else {
         // window.location.hash = '/file'
         document.head.querySelector('title').innerHTML = 'File'
         let cssTag = document.createElement('link')
         cssTag.rel = "stylesheet"
-        cssTag.href = "https://cdn.jsdelivr.net/gh/thuno/goline2@cda4f66/screen/module/file/design-view.css"
+        cssTag.href = "https://cdn.jsdelivr.net/gh/thuno/goline2@aacfce7/screen/module/file/design-view.css"
         // cssTag.href = "./screen/module/file/design-view.css"
         document.head.appendChild(cssTag)
         // $('body').load('./screen/module/file/design-view.html')
-        $('body').load('https://cdn.jsdelivr.net/gh/thuno/goline2@cda4f66/screen/module/file/design-view.html')
+        $('body').load('https://cdn.jsdelivr.net/gh/thuno/goline2@aacfce7/screen/module/file/design-view.html')
     }
 } else {
     window.location.hash = '/login'
     document.head.querySelector('title').innerHTML = 'Login'
     let cssTag = document.createElement('link')
     cssTag.rel = "stylesheet"
-    cssTag.href = "https://cdn.jsdelivr.net/gh/thuno/goline2@cda4f66/screen/module/login/login-view.css"
+    cssTag.href = "https://cdn.jsdelivr.net/gh/thuno/goline2@aacfce7/screen/module/login/login-view.css"
     document.head.appendChild(cssTag)
-    $('body').load('https://cdn.jsdelivr.net/gh/thuno/goline2@cda4f66/screen/module/login/login-view.html')
+    $('body').load('https://cdn.jsdelivr.net/gh/thuno/goline2@aacfce7/screen/module/login/login-view.html')
 }
